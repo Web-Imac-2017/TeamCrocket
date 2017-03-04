7ff274515a2bdcf2278f402c29e1b525f02e304a
METTER-ROTHAN <jmetterrothan@gmail.com>
2017-03-04 22:26:13 +0100

    Suppression include index dans api

--- api.php
+++ api.php
@@ -11,41 +11,37 @@ require_once(ROOT_INC . 'init.php');
 // API
 $request = $_GET['request'] ?? '';
 
-if($request != NULL && preg_match('#api\/(.*)\/(.*)(?:\/(.*)\/?)?$#iU', $request, $matches)){
-    $export = array();
+$request != NULL && preg_match('#api\/(.*)\/(.*)(?:\/(.*)\/?)?$#iU', $request, $matches);
+$export = array();
 
-    $route = NULL;
-    $controller = NULL;
-    $task = NULL;
-    $args = [];
+$route = NULL;
+$controller = NULL;
+$task = NULL;
+$args = [];
 
-    array_shift($matches);
-    @list($controller, $task, $args) = $matches;
+array_shift($matches);
+@list($controller, $task, $args) = $matches;
 
-    try{
-        $class = "\App\\Controller\\".ucfirst($controller) . 'Controller';
+try{
+    $class = "\App\\Controller\\".ucfirst($controller) . 'Controller';
 
-        if(!class_exists($class)){
-            throw new Exception(sprintf(gettext("Invalid controller '%s'"), $controller));
-        }
-        if(!method_exists($class, $task)){
-            throw new Exception(sprintf(gettext("Invalid task '%s'"), $task));
-        }
-
-        $temp = new $class();
-
-        $export['output'] = $temp->$task(...explode('/', $args));
-        $export['success'] = true;
+    if(!class_exists($class)){
+        throw new Exception(sprintf(gettext("Invalid controller '%s'"), $controller));
     }
-    catch(Exception $e){
-        $export['message'] = $e->getMessage();
-        $export['success'] = false;
+    if(!method_exists($class, $task)){
+        throw new Exception(sprintf(gettext("Invalid task '%s'"), $task));
     }
 
-    header('Content-Type: application/json');
-    echo json_encode($export);
-    die();
+    $temp = new $class();
+
+    $export['output'] = $temp->$task(...explode('/', $args));
+    $export['success'] = true;
 }
-else{
-    include ROOT.'index.php';
+catch(Exception $e){
+    $export['message'] = $e->getMessage();
+    $export['success'] = false;
 }
+
+header('Content-Type: application/json');
+echo json_encode($export);
+die();
