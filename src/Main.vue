d0836dc962be6fe253f632598a2735aaadede087
Marion Prigent <mar.laur@hotmail.fr>
2017-03-05 11:48:09 +0100

    modif connexion

--- src/Main.vue
+++ src/Main.vue
@@ -1,5 +1,7 @@
+
 <template>
-  <body>
+  
+  <div id="main-index">
    
     
       <img src="./assets/Meetic.png" width="250ox" id="logo" />
@@ -13,10 +15,55 @@
       <button type="button" id="button_connexion" class="button_style">SE CONNECTER</button>
       <button type="button" id="button_inscription" class="button_style">S'INSCRIRE</button>
 
-  </body>
+      <div id="connexion">
+       <div id ="form_connexion">
+          <form action="http://api.meowtic.com/user/login" method="post" class="form">
+              <fieldset>
+               <ul>
+                <li>
+                  <label for="pseudo">MAIL</label>              
+                  <input id="pseudo" name="email" type="email">
+                </li>
+                 <div class="trait">
+                 </div>
+                <li>  
+                  <label for="pseudo">MOT DE PASSE</label>              
+                  <input id="pseudo" name="password" type="password">
+                </li>
+                </ul>
+                <div class="trait">
+                </div>
+                <div id="pw_lost">Mot de passe perdu?</div>
+              </fieldset>
+                <button type="submit" id="button_validation" class="button_style">VALIDER
+                <input type="hidden" name="token" value="temp"> 
+              
+                </br>
+                <img src="./assets/search_mob.png"/>
+                </button>
+          </form>
+       </div>
+
+      
+      </div>  
+
+  </div>
 </template>
 
 <script>
+    
+    
+   
+ var temp = (window.location.pathname).trim();
+    if(temp.charAt(0) == '/'){
+     temp = temp.substr(1);
+    }
+
+    if(temp.charAt(temp.length - 1) == '/'){
+    temp = temp.substr(0, temp.length - 1);
+    }
+    console.log(temp.split('/')); 
+    alert(temp[1]);
 
 
 </script>
@@ -25,6 +72,7 @@
 @val : 15px; /* less example */
 @valBleuF : #212D48;
 @valBleuC : #49556d;
+@valGrisC : #edeef0;
 
 
 body {
@@ -64,22 +112,44 @@ body {
 
 #button_connexion{
   margin-top:55px;
+  width:300px;
+  font-size:1.3em;
 }
 
 
 #button_inscription{
     margin-top:45px;
+    width:300px;
+    font-size:1.3em;
+}
+
+#connexion{
+ /* display:none;*/
+}
+
+#button_validation{
+    width:180px;
+    height:80px;
+    font-size:1.6em;
+    background-color:rgba(55, 68, 94, 0.9);
+    box-shadow : 2px 2px 2px rgba(0, 0, 0, 0.3);
+    margin-bottom: 15px;
+
+}
+
+#button_validation img{
+  width:30px;
+  padding-top:3px;
 }
 
 .button_style{
-   width:300px;
-  /*border-radius: 6px;*/
+
+   border-radius: 6px;
    letter-spacing: 0.2em;
    padding:5px;
    background-color:@valBleuC;
    display: block;
    margin: auto;
-   font-size:1.3em;
    font-family: 'Moon';
    font-weight : bold;
    color:white;
@@ -95,6 +165,59 @@ body {
   transition: all 580ms ease-in-out;
   /*border: 1px solid rgba(#fff, 0);*/
   bottom: 0px;
+}
+
+#form_connexion{
+  margin:auto;
+  width:400px;
+  margin: auto;
+  margin-top:-170px;
+  background-color:white;
+  border-radius: 6px;
+  box-shadow : 7px 7px 4px rgba(0, 0, 0, 0.3);
+  
+}
+
+#form_connexion label{
+  font-size:1.7em;
+  font-family: 'Moon';
+  font-weight : lighter;
+  color:@valBleuC;
+  letter-spacing: 0.1em;
+  padding-left:20px;
+
+}
+
+.trait{
+ 
+   width:320px;
+   background-color:#dbdbdb;
+   margin-top:5px;
+   margin-left:28px;
+   height:2px;
+}
+
+ fieldset{
+  border:none;
+}
+
+ li #pseudo{
+ width:330px;
+ height:30px;
+ display:block;
+ border-radius: 10px;
+ margin-top:5%;
+ margin-left:10px;
+ background-color:@valGrisC;
+ }
+
+#main-index li {
+  padding-top:25px;
+}
+
+#pw_lost{
+  text-align:right;
+  margin-right:25px
 }
 
 @media screen and (max-width: 575px ) {
