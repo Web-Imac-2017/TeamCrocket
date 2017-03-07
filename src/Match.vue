0a0b7539cbe93fd13fd8dd5bd2aaee9a02dd4c3c
Melodie Mersch <melodie.mersch@gmail.com>
2017-03-07 14:54:35 +0100

    Add Match.vue

---
+++ src/Match.vue
@@ -0,0 +1,110 @@
+<template>
+    <section id="frame">
+      <img src="./assets/Meetic.png" width="110ox"/>
+      <h1>Match</h1>
+      <h2>Trouvez le profil qui fera chavirer votre coeur</h2>
+
+      <div class="profile">
+        <img src="./assets/cat.jpg"/>
+        <div>
+          <span>Pattenrond</span>
+          <ul>
+            <li>1 an</li>
+            <li>Espiègle</li>
+            <li>Aime dormir</li>
+            <li>N'aime pas l'eau</li>
+          </ul>
+        </div>
+      </div>
+      <div id="choice">
+        <a class="accept" href="">accept</a>
+        <a class="refuse" href="">refuse</a>
+      </div>
+    </section>
+</template>
+
+
+<style lang="less">
+
+ @import "definitions"; /* import common definitions */
+
+  #frame {
+    display: block;
+    width:60%;
+    margin: auto;
+    margin-top: 2%;
+    padding:3%;
+    background:white;
+    text-align: center;
+  }
+
+  .profile {
+    display: flex;
+    flex-direction: row;
+    width: 80%;
+    margin: auto;
+    margin-top: 2em;
+    border: 1px solid #DADADA;
+
+    img {
+      height: 40%;
+      width: 40%;
+    }
+
+    div {
+      padding: 4%;
+      height: 46%;
+      width: 46%;
+
+      text-align: left;
+      color: @darkBlue;
+
+      span {
+        display: block;
+        text-transform: uppercase;
+        font-weight:bolder;
+        font-size: 1.1em;
+        text-align: center;
+        margin-bottom: 0.7em;
+      }
+
+      ul li {
+        display: block;
+        font-weight: 100;
+      }
+    }
+
+    @media (max-width: 700px)
+    {
+      flex-direction: column;
+      width: 40%;
+
+      img, div {
+        width: 100%;
+      }
+    }
+  }
+
+  #choice {
+    display: flex;
+    width: 60%;
+    margin: auto;
+
+    a {
+      height: 10em;
+      width: 10em;
+      line-height: 10em;
+      border-radius: 100%;
+      color: @lightBlue;
+      margin: 1em;
+    }
+
+    a.accept {
+      background-color: #0F0;
+    }
+
+    a.refuse {
+      background-color: #F00;
+    }
+  }
+</style>
