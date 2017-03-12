ab50a998326e29c56fe444b824200273fc7ce104
Marion Prigent <mar.laur@hotmail.fr>
2017-03-12 15:14:44 +0100

    ajout profil animal

---
+++ src/components/ProfileAnimal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="content_profile">
+      
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style lang="less">
+@import "../definitions"; /* import common definitions */
+
+</style>
