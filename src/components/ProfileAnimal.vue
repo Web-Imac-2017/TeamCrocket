28708afd69fd075424f7485459113866bcd57325
Marion Prigent <mar.laur@hotmail.fr>
2017-03-12 17:05:08 +0100

    ajout profileanimal

--- src/components/ProfileAnimal.vue
+++ src/components/ProfileAnimal.vue
@@ -1,10 +1,112 @@
 <template>
   <div id="content_profile">
-      
+          <h1>Profil</h1>
+          <div id="cover">
+
+
+          </div>
+          <div id="content_info">
+            <div class="details">
+              <div id="right">
+                <h1>Details</h1>
+                <ul>
+                  <li>
+                    <h2>Nom</h2>
+                    <span>{{userForm.nickname}}</span>
+                  </li>
+                  <li>
+                    <h2>Sexe</h2>
+                    <span>{{userForm.sex}}</span>
+                  </li>
+                  <li>
+                    <h2>City</h2>
+                    <span>{{userForm.city}}</span>
+                  </li>
+                </ul>
+              </div>
+              <div id="left">
+                <ul>
+                  <li>
+                    <h2>Age</h2>
+                    <span>{{userForm.age}} ans</span>
+                  </li>
+                  <li>
+                    <h2>Naissance</h2>
+                    <span>{{userForm.date_birth}}</span>
+                  </li>
+                </ul>
+              </div>
+
+
+            </div>
+            <div class="desc">
+              <h1>Description</h1>
+              <ul>
+                <li>
+                  <h2>Ce qu'il Aime</h2>
+                  <span id="detail_lastname">{{userForm.like}}</span>
+                </li>
+                <li>
+                  <h2>Ce qu'il Aime</h2>
+                  <span id="detail_lastname">{{userForm.dislike}}</span>
+                </li>
+              </ul>
+              <h1>Caracteres</h1>
+                <h3>{{caracForm.name}}</h3>
+            </div>
+
+          </div>
   </div>
 </template>
 
 <script>
+
+import Vue from 'vue'
+
+Vue.use(require('vue-resource'));
+
+export default {
+  data() {
+    return {
+      user : null,
+      userForm : {
+          nickname:'',
+          sex:'',
+          like:'',
+          dislike:'',
+          city: '',
+          date_birth: '',
+          age: '',
+
+
+      },
+
+      caracForm : {
+        name:'fr'
+      }
+
+    }
+  },
+
+    created : function(){
+      this.$http.get('https://api.meowtic.com/user/list_animal')
+        .then(function(response){
+          let data = response.data;
+          this.user = response.data.output.data;
+          if(data.success){
+            this.userForm.nickname = this.user[0].name;
+            this.userForm.sex = this.user[0].sex;
+            this.userForm.like = this.user[0].like;
+            this.userForm.dislike = this.user[0].dislike;
+            this.userForm.city = this.user[0].city;
+            this.userForm.date_birth = this.user[0].date_birth;
+            this.userForm.age = this.user[0].age;
+            this.caracForm.name = this.user[0].characteristics[0].name
+          }
+        })
+    }
+
+}
 
 
 </script>
@@ -12,4 +114,32 @@
 <style lang="less">
 @import "../definitions"; /* import common definitions */
 
+#content_info .details h1{
+  margin-top:0.2em;
+}
+#content_info .desc h1{
+  margin-top:0.2em;
+}
+#content_info .details ul li h2{
+  margin-top:3em;
+}
+#content_info .desc ul li h2{
+  margin-top:3em;
+}
+
+#content_info .desc h3{
+  margin-top:2em;
+}
+
+#content_info span{
+
+}
+
+#right{
+  float:left;
+}
+
+#left{
+  float:right;
+}
 </style>
