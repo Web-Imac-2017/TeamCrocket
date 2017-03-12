f230e2ded32f9615b816d3373830fba1510b868a
Melodie Mersch <melodie.mersch@gmail.com>
2017-03-12 19:56:34 +0100

    Changes in SignUp.vue + Date.vue added

---
+++ src/components/Date.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="date_birth">
+    <select name="date_birth[day]">
+        <option  v-for="day in days" :value="day">{{day}}</option>
+    </select>
+    <select name="date_birth[month]">
+        <option  v-for="month in months" :value="month">{{month}}</option>
+    </select>
+    <select name="date_birth[year]">
+        <option  v-for="year in years" :value="year">{{year}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+Vue.use(require('vue-resource'));
+
+export default {
+  data() {
+    return {
+      years : [],
+      months : [],
+      days: [],
+    }
+  },
+  created : function()
+  {
+    var start = 1920;
+    var end =  new Date().getFullYear() - 13;
+    var i = 0;
+
+    while (i < (end - start))
+    {
+      this.years[i] = start + i;
+      ++i;
+    }
+
+    for (i = 0; i < 12; ++i)
+    {
+      this.months[i] = i + 1;
+    }
+
+    for (i = 0; i < 31; ++i)
+    {
+      this.days[i] = i + 1;
+    }
+  }
+}
+
+
+</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,7 +2,7 @@
   <section>
     <h2>Informations du proprietaire</h2>
 
-    <form v-on:submit.prevent="signup" id="signup-form">
+    <form v-on:submit.prevent="submit" id="signup-form">
       <ul>
         <li>
           <label>Pseudo (entre 4 et 20 caractères - lettres, chiffres - _ ou . acceptés)*</label>
@@ -10,16 +10,18 @@
         </li>
         <li>
           <label>Mail*</label>
-          <input type="text" v-model="signupForm.email">
+          <input type="email" v-model="signupForm.email">
         </li>
         <li>
-          <label>Mot de passe (les contraintes)*</label>
-          <input type="text" v-model="signupForm.password">
+          <label>Mot de passe (min 8 caractères, minuscule, majuscule, caractère spécial, chiffres au minimum 1 fois)*</label>
+          <input type="password" v-model="signupForm.password" required pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
         </li>
+
         <li>
           <label>Date de naissance*</label>
-          <input type="date" v-model="signupForm.date_birth">
+          <date-component></date-component>
         </li>
+
         <li>
           <div class="sex">
             <input type="radio" id="m" value="m" v-model="signupForm.sex">
@@ -42,53 +44,65 @@
 </template>
 
 <script>
+import Vue from 'vue'
+Vue.use(require('vue-resource'));
+
+import DateComponent from "./Date.vue"
+
 export default {
-data(){
-  return{
-    rcapt_sig_key: "6LcIPBUUAAAAAL7aFlWT0BNXe6nNKbRUTvQNrhXg",
-    rcapt_id: 0, // will be used later
-    user: null,
-    signupForm: {
-      id: 0,
-      nickname: '',
-      password: '',
-      email: '',
-      sex: '',
-      position : {lat: 0, lng: 0},
-      date_birth: '',
-      "g-recaptcha-response": ''
+  components: {
+    DateComponent
+  },
+  data(){
+    return{
+      rcapt_sig_key: "6LcIPBUUAAAAAL7aFlWT0BNXe6nNKbRUTvQNrhXg",
+      rcapt_id: 0, // will be used later
+      user: null,
+      signupForm: {
+        id: 0,
+        nickname: '',
+        password: '',
+        email: '',
+        sex: '',
+        position : {lat: 0, lng: 0},
+        date_birth: '',
+        "g-recaptcha-response": ''
+      }
     }
-  }
-},
+  },
 
-mounted: function() {
-  if (window.grecaptcha) {
-    this.rcapt_id = grecaptcha.render(document.getElementsByClassName('g-recaptcha')[0], { sitekey : this.rcapt_sig_key });
-  }
-  var g_recaptcha_response = grecaptcha.getResponse(this.rcapt_id);
+  mounted: function() {
+    if (window.grecaptcha) {
+      this.rcapt_id = grecaptcha.render(document.getElementsByClassName('g-recaptcha')[0], { sitekey : this.rcapt_sig_key });
+    }
+    var g_recaptcha_response = grecaptcha.getResponse(this.rcapt_id);
 
-  if (g_recaptcha_response.length == 0) {
-     this.error = "Complete captcha challenge";
-     return
-  }
-},
+    if (g_recaptcha_response.length == 0) {
+       this.error = "Complete captcha challenge";
+       return
+    }
+  },
 
-methods: {
-  submit : function(){
-    this.signupForm["g-recaptcha-response"] = grecaptcha.getResponse(this.rcapt_id);
-    this.$http.post('https://api.meowtic.com/user/edit', this.signupForm, { emulateJSON : true })
-      .then(function(response){
-        let data = response.data;
-        if(data.success){
-          console.log('Profile créé');
-          this.user = response.data.output;
-        }
-        else{
-          alert(response.data.message);
-        }
-    })
-  }
-},
+  methods: {
+    submit : function(){
+      this.signupForm.date_birth = document.getElementById("date_birth").value;
+      this.signupForm["g-recaptcha-response"] = grecaptcha.getResponse(this.rcapt_id);
+
+      this.$http.post('https://api.meowtic.com/user/edit', this.signupForm)
+        .then(function(response){
+          let data = response.data;
+          if(data.success){
+            console.log('Profile créé');
+            this.user = response.data.output;
+            alert(document.getElementById("date_birth").value);
+            location.href = 'profileuser';
+          }
+          else{
+            alert(response.data.message);
+          }
+      })
+    }
+  },
 }
 </script>
 
