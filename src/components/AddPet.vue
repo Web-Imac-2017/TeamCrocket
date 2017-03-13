90e79fdd601eca86a7afc7dba6e0dca9c30f8ba2
Sophie Notlas <sophie.notlas@gmail.com>
2017-03-13 10:14:46 +0100

    Ajout d'un animal ok avec CSS

--- src/components/AddPet.vue
+++ src/components/AddPet.vue
@@ -1,16 +1,18 @@
 <template>
 
-    <section>
-      <h2>Informations du proprietaire</h2>
+    <section id="addpet">
+      <h1><img src="../assets/patoune.png"><br/>
+          Ajouter un animal</h1>
 
       <form v-on:submit.prevent="addpet" id="signup-form">
         <ul>
           <li>
-            <label>Pseudo (entre 4 et 20 caractères - lettres, chiffres - _ ou . acceptés)*</label>
-            <input type="text" name="name" v-model="addPet.name">
+            <h2>Pseudo</h2>
+            <input type="text" name="name" class="pseudo" v-model="addPet.name">
           </li>
 
             <li>
+              <h2>Espece</h2>
               <select name="species_id" v-model="addPet.species_id">
                   <option value="6">Chat</option>
                   <option value="7">Chien</option>
@@ -19,9 +21,12 @@
         </ul>
       </form>
 
-    <div>
-      <button v-on:click="submit()">Valider</button>
-    </div>
+      <div class="button_validation" >
+        <button class="button_style"  v-on:click="submit()">VALIDER
+          <img src="../assets/search_mob.png" class="img_button"/>
+        </button>
+      </div>
+
 
     </section>
 
@@ -60,8 +65,42 @@ export default {
 
 <style lang="less">
 @import "../definitions";
-
+#addpet{
+  background-color: white;
+  width: 70%;
+  padding: 3em;
+  margin:auto;
+  margin-top:2em;
+  margin-bottom:5em;
+}
 #signup-form {
   text-align: center;
+}
+form#signup-form ul li input.pseudo{
+  width:400px;
+  margin:auto
+}
+form#signup-form ul li select{
+  margin:auto;
+  background-color:white;
+  border-radius: 6px;
+  box-shadow : 7px 7px 4px rgba(0, 0, 0, 0.3);
+  width: 100px;
+  font-size: 1em;
+  padding:0.5em;
+  padding-top:0.2em;
+  padding-bottom:0.2em;
+  margin-bottom:3em;
+  color:@darkBlue;
+  font-family: @fontTitle;
+}
+@media screen and (max-device-width:650px), screen  and (max-width:650px){
+  #addpet{
+    width: 90%;
+    padding: 1em;
+  }
+  form#signup-form ul li input.pseudo{
+    width:100%
+  }
 }
 </style>
