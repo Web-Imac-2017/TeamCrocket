8700db94c6a0a9b951fd48d5bba30f2c0e828749
Marion Prigent <mar.laur@hotmail.fr>
2017-03-13 23:40:34 +0100

    css du SignUp

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
-    <h2>Informations du proprietaire</h2>
+    <div id="form-inscription">
+    <h1>Informations du proprietaire</h1>
 
     <form v-on:submit.prevent="signup" id="signup-form">
       <input type="hidden" name="id" value="0">
@@ -8,29 +9,38 @@
       <input type="hidden" name="longitude" :value="long">
       <ul>
         <li>
-          <label>Pseudo*</label>
+          <label>Pseudo</label>
           <input type="text" name="nickname" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{3,20}$" title="entre 4 et 20 caractères - lettres, chiffres - _ ou . acceptés">
+          <div class="trait"></div>
         </li>
         <li>
-          <label>Mail*</label>
+          <label>Mail</label>
           <input type="email" name="email">
+          <div class="trait"></div>
         </li>
         <li>
-          <label>Mot de passe*</label>
+          <label>Mot de passe</label>
           <input type="password" name="password" required pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" title="min 8 caractères, minuscule, majuscule, caractère spécial, chiffres au minimum 1 fois">
+          <div class="trait"></div>
         </li>
 
         <li>
-          <label>Date de naissance*</label>
+          <label>Date de naissance</label>
           <date-component></date-component>
         </li>
 
         <li>
           <div class="sex">
-            <input type="radio" id="m" value="m" name="sex">
-            <label for="m"><img src="../assets/man.png" alt="Homme"/></label>
-            <input type="radio" id="f" value="f" name="sex">
+            <ul>
+              <li>
+                  <label for="m"><img src="../assets/man.png" alt="Homme"/></label>
+                  <input type="radio" id="m" value="m" name="sex">
+              </li>
+              <li>
             <label for="f"><img src="../assets/woman.png" alt="Logo"/></label>
+              <input type="radio" id="f" value="f" name="sex">
+              </li>
+            </ul>
           </div>
         </li>
         <li>
@@ -38,10 +48,10 @@
         </li>
       </ul>
       <div>
-        <button type="submit">Valider</button>
       </div>
     </form>
-
+    <button type="submit">Valider</button>
+  </div>
   </section>
 </template>
 
@@ -106,7 +116,68 @@ created: function() {
 <style lang="less">
 @import "../definitions";
 
-#signup-form {
-  text-align: center;
+
+#form-inscription{
+  width:600px;
+  margin:auto;
+  margin: auto;
+  background-color:white;
+  border-radius: 6px;
+  box-shadow : 7px 7px 4px rgba(0, 0, 0, 0.3);
 }
+#form-inscription label{
+  font-size:1.3em;
+  font-family: 'Moon';
+  font-weight : lighter;
+  color:@lightBlue;
+  letter-spacing: 0.1em;
+  padding-left:11%;
+  float:left;
+}
+
+#form-inscription h1{
+  font-size: 1.5em;
+  padding-top:1em
+}
+
+ #form-inscription input{
+   width:80%;
+   margin-left: 10%;
+   height:2em;
+   display:block;
+   border-radius: 10px;
+   background-color:@lightGrey;
+ }
+ div#date_birth{
+  float:left;
+  margin-left: 2em;
+  margin-top:1.1em;
+
+ }
+
+ div.sex{
+
+   margin-top: -2em;
+   margin-right: 3em;
+
+ }
+
+ div.sex ul li{
+      display: inline-block;
+      margin-right: 1em;
+      margin-top: -1em;
+
+ }
+
+ .g-recaptcha{
+margin-left: 9.3em;
+padding-bottom: 3em;
+ }
+
+ div#form-inscription button{
+   z-index: 100;
+   margin-top:-1em;
+   margin-left: 7em;
+   position: absolute;
+ }
 </style>
